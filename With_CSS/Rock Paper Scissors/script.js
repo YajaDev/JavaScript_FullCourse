37874bd135d3265535f69af1a45fdb8a9a7adf8f
YajaDev <ajay.tomagan.10212002@gmail.com>
2025-07-17 14:31:26 +0800

    final RockPaperScissor

--- With_CSS/Rock Paper Scissors/script.js
+++ With_CSS/Rock Paper Scissors/script.js
@@ -12,15 +12,55 @@ displayScore();
 
 function pickRandomItem(){
     const item = ['Paper','scissor','Rock'];
-    computerPicked = item[Math.floor(Math.random() * item.length)];
+    return item[Math.floor(Math.random() * item.length)];
 }
 
 function displayScore(){
     document.querySelector('#js-score').innerText = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
 
+document.querySelector('#rock-btn')
+    .addEventListener('click', () => {
+        playGame('Rock')
+    })
+
+document.querySelector('#paper-btn')
+    .addEventListener('click', () => {
+        playGame('Paper')
+    })
+
+document.querySelector('#scissor-btn')
+    .addEventListener('click', () => {
+        playGame('Scissor')
+    });
+
+document.querySelector('.reset-btn')
+    .addEventListener('click', () => {
+        showResetConfirmation();
+    });
+
+document.querySelector('.autoPlay-btn')
+    .addEventListener('click', () => {
+        autoPlay();
+    });
+
+document.body.addEventListener('keydown', (event) => {    
+    if (event.key === 'r') {
+        playGame('Rock')
+    } else if (event.key === 's') {
+        playGame('Scissor')
+    } else if (event.key === 'p') {
+        playGame('Paper')   
+    } else if (event.key === 'a') {
+        autoPlay();
+    } else if (event.key === 'Backspace') {
+        showResetConfirmation();
+    }
+});
+
 function playGame(pick){
     userPicked = pick
+    computerPicked = pickRandomItem();
 
     if (computerPicked === userPicked){
         result = 'Tie!';
@@ -36,6 +76,7 @@ function playGame(pick){
     }
 
     localStorage.setItem('score', JSON.stringify(score));
+
     //display to html
     document.querySelector('#js-moves').innerHTML = `
     You 
@@ -43,8 +84,32 @@ function playGame(pick){
     <img src="images/${computerPicked}-emoji.png" alt="${computerPicked}" class="move-icon"> 
     Computer`;
     document.querySelector('#js-result').innerText = result;
+
     displayScore();
+
+    //check score on console
     console.log(JSON.parse(localStorage.getItem('score')));
+}
+
+function showResetConfirmation() {
+    document.getElementById('reset_msg_container').innerHTML =`
+    <p>Are you sure you want to reset the score?</p>
+    <button id="yes-btn" class="reset-mgs-btn">Yes</button>
+    <button id="no-btn" class="reset-mgs-btn">No</button>
+    `;
+
+    document.querySelector('#yes-btn').addEventListener('click',() => {
+        resetScore();
+        hideResetConfirmation();
+    });
+
+    document.querySelector('#no-btn').addEventListener('click',() => {
+        hideResetConfirmation();
+    });
+}
+
+function hideResetConfirmation() {
+    document.getElementById('reset_msg_container').innerHTML = '';
 }
 
 function resetScore() {
@@ -54,4 +119,22 @@ function resetScore() {
     localStorage.setItem('score', JSON.stringify(score));
     displayScore();
     console.log(JSON.parse(localStorage.getItem('score')));
+}
+
+let isAutoPlay = false;
+let intervalId;
+
+function autoPlay() {
+    if (!isAutoPlay) {
+        intervalId = setInterval(function () {
+            userPicked = pickRandomItem();
+            playGame(userPicked);
+        }, 1200);
+        document.querySelector('.autoPlay-btn').innerText = 'Stop Playing';
+        isAutoPlay = true;
+    }else {
+        clearInterval(intervalId);
+        document.querySelector('.autoPlay-btn').innerText = 'Auto Play';
+        isAutoPlay = false;
+    }
 }
